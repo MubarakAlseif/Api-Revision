7dbcc5ce026bedbf894f324107f0b0191a8760f5
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-12-19 14:22:23 +0300

    done.

--- Jokes/ViewController.swift
+++ Jokes/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
+struct Joke : Decodable {
+    
+    let jokes : [Jokes]
+    let available : Int
+    
+}
+
+struct Jokes : Decodable {
+    
+    let id : Int
+    let joke : String
+    
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        request()
     }
 
-
+    func request () {
+ let url = URL(string: "https://api.humorapi.com/jokes/search?api-key=6d484bb7f09047b395d2e5d0801d9751")
+        
+        let request = URLRequest(url: url!)
+        
+        URLSession.shared.dataTask(with: request) { data , response , error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+//                let data = String(data: data! , encoding: String.Encoding.utf8)
+//                print(data)
+                if let data = data {
+                    do {
+                        let newJoke = try? JSONDecoder().decode(Joke.self, from: data)
+                        print(newJoke?.available)
+                    } catch {
+                       print(error)
+                    }
+                }
+                
+            }
+        } .resume()
+        
+        
+       }
+  
 }
-
